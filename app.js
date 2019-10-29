f449f06f4d749e76945c6a051ee6bb64f2ccd10a
Jorge <jorrillop@hotmail.com>
2019-10-29 13:06:03 +0100

    Design

--- app.js
+++ app.js
@@ -5,19 +5,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-
-// view engine setup
-// mongoose.connect('mongodb://localhost/agenciaViajes', {
-//   keepAlive: true,
-//   useNewUrlParser: true,
-//   reconnectTries: Number.MAX_VALUE
-// });
-
 
 mongoose.connect(process.env.MONGODB_URI, {
   keepAlive: true,
@@ -36,28 +27,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-// NOTE: requires a views/not-found.ejs template
+
 app.use((req, res, next) => {
   res.status(404);
   res.render('not-found');
 });
 
-// const host = process.env.HOST || '0.0.0.0';
-// const port = process.env.PORT || 3000;
-// app.listen(port, host, () => {
-
-//   console.log('El servidor funciona')
-// })
-// NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
-  // always log the error
+
   console.error('ERROR', req.method, req.path, err);
 
-
-  // only render if the error ocurred before sending the response
   if (!res.headersSent) {
     res.status(500);
     res.render('error');
